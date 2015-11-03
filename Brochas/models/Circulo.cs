ba1dac3337da7524097593e80f15fd118292e221
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-11-02 19:21:16 -0500

    Creacion del proyecto y repositorio

---
+++ Brochas/models/Circulo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brochas.models
+{
+    public class Circulo
+    {
+        public long Width { get; set; }
+        public long Height { get; set; }
+    }
+}
